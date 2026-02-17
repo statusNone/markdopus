95d991573fb365423215d00d3996deba801eaaa6
Race Malin <racemalin@protonmail.com>
2026-02-17 10:31:01 -0800

    add leafnode and tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -21,6 +21,42 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_not_eq(self):
         self.assertNotEqual(self.node0, self.node1)
+
+
+
+class TestLeafNode(unittest.TestCase):
+    def setUp(self):
+        self.node0 = LeafNode(None, None)
+        self.node1 = LeafNode("p", "Hello")
+        self.node2 = LeafNode("a", "Click", {"href": "https://google.com"})
+        self.node3 = LeafNode("p", "Hello", {"class": "intro"})
+        self.node4 = LeafNode("p", "Hello", {"class": "intro"})
+        self.node5 = LeafNode(None, "Just some raw text here")
+
+
+    def test_leaf_to_html_p(self):
+        self.assertEqual(self.node1.to_html(), "<p>Hello</p>")
+
+
+    def test_eq(self):
+        self.assertEqual(self.node3, self.node4)
+
+
+    def test_to_html_no_tag(self):
+        self.assertEqual(self.node5.to_html(), "Just some raw text here")
+
+
+    def test_to_html_with_props(self):
+        self.assertEqual(self.node2.to_html(), '<a href="https://google.com">Click</a>')
+
+
+    def test_to_html_no_value(self):
+        with self.assertRaises(ValueError):
+            self.node0.to_html()
+
+
+    def test_not_eq(self):
+        self.assertNotEqual(self.node1, self.node2)
 
 
 if __name__ == "__main__":
